9a72955b0fc443de90f18b44d7a432b95e68aa29
Kappanneo <pannacciclaudio@gmail.com>
2018-10-04 18:32:31 +0200

    custom resources

--- test/Device/coap-server.py
+++ test/Device/coap-server.py
@@ -7,47 +7,20 @@ import aiocoap.resource as resource
 import aiocoap
 
 
-class BlockResource(resource.Resource):
-    """Example resource which supports the GET and PUT methods. It sends large
-    responses, which trigger blockwise transfer."""
+class SimpleResource(resource.Resource):
+    """Example resource which supports the GET and PUT methods."""
 
     def __init__(self):
         super().__init__()
-        self.set_content(b"This is the resource's default content. It is padded "\
-                b"with numbers to be large enough to trigger blockwise "\
-                b"transfer.\n")
-
-    def set_content(self, content):
-        self.content = content
-        while len(self.content) <= 1024:
-            self.content = self.content + b"0123456789\n"
+        self.content= b"this is an important string\n"
 
     async def render_get(self, request):
         return aiocoap.Message(payload=self.content)
 
     async def render_put(self, request):
         print('PUT payload: %s' % request.payload)
-        self.set_content(request.payload)
+        self.content= request.payload
         return aiocoap.Message(code=aiocoap.CHANGED, payload=self.content)
-
-
-class SeparateLargeResource(resource.Resource):
-    """Example resource which supports the GET method. It uses asyncio.sleep to
-    simulate a long-running operation, and thus forces the protocol to send
-    empty ACK first. """
-
-    def get_link_description(self):
-        # Publish additional data in .well-known/core
-        return dict(**super().get_link_description(), title="A large resource")
-
-    async def render_get(self, request):
-        await asyncio.sleep(3)
-
-        payload = "Three rings for the elven kings under the sky, seven rings "\
-                "for dwarven lords in their halls of stone, nine rings for "\
-                "mortal men doomed to die, one ring for the dark lord on his "\
-                "dark throne.".encode('ascii')
-        return aiocoap.Message(payload=payload)
 
 class TimeResource(resource.ObservableResource):
     """Example resource that can be observed. The `notify` method keeps
@@ -77,7 +50,7 @@ class TimeResource(resource.ObservableResource):
 
     async def render_get(self, request):
         payload = datetime.datetime.now().\
-                strftime("%Y-%m-%d %H:%M").encode('ascii')
+                strftime("%Y-%m-%d %H:%M\n").encode('ascii')
         return aiocoap.Message(payload=payload)
 
 # logging setup
@@ -92,9 +65,8 @@ def main():
     root.add_resource(('.well-known', 'core'),
             resource.WKCResource(root.get_resources_as_linkheader))
     root.add_resource(('time',), TimeResource())
-    root.add_resource(('other', 'block'), BlockResource())
-    root.add_resource(('other', 'separate'), SeparateLargeResource())
-
+    root.add_resource(('resource',), SimpleResource())
+    
     asyncio.Task(aiocoap.Context.create_server_context(root))
 
     asyncio.get_event_loop().run_forever()
